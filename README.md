ffe273cebfba95aa5e48f1a9ee1d5ae0f945188a
commanderHR1 <39062223+commanderHR1@users.noreply.github.com>
2019-01-11 20:59:56 -0500

    Updated python-password-generator

--- README.md
+++ README.md
@@ -1,12 +1,8 @@
 # python-password-generator
-Generates a random password using python. 
+Generates a random password using python.
 
-
-
-How To Install:
+For instructions on how to install, read the corresponding file in the main folder of this repository for your OS.
 
 For Linux users:
-Install the python package using your package manager. Then, copy the .py file to a convient location. You can then use the command 
+Install the python package using your package manager. Then, copy the .py file to a convenient location. You can then use the command
 python python-password-generator.py to run the file. 
-
-
---
+++ python_password_generator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+import random;
+import sys;
+
+#Defines all possible characters for the password
+chars= "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()=+-~/[]{};:<>`"
+#Gets user input
+input("Press any button to continue...")
+length=input("Password Length?");
+#Changes user input to an integer value
+length = int(length)
+#Defines the password variable
+password = ''
+
+#Determines if the length is valid, then generates password, else, displays error
+if (length > 0):
+    for c in range(length):
+     password += random.choice(chars)
+else:
+    print('Error! Invalid Length!')
+
+#Displays the password
+print ("PASSWORD: %s" % password)
