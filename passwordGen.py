d9d12561a4d63cb6a05f47215bafe7b099bc1eaa
Harris Ransom <hransom528@gmail.com>
2020-07-14 16:58:14 -0400

    Removed package files and refactored code

--- passwordGen.py
+++ passwordGen.py
@@ -4,13 +4,13 @@ import secrets
 import string
 import random
 
-#Defines all possible characters for the password
+# Defines all possible characters for the password
 lowercase = string.ascii_lowercase
 uppercase = string.ascii_uppercase
 nums = string.digits
 special_char = "!@#$%^&*()=+-~/[]{};:<>`"
 
-#Defines global variables
+# Defines misc. global variables
 password = ''
 count = 0
 length_error = False
@@ -18,14 +18,14 @@ capital_error = False
 num_error = False
 special_error = False
 
-#Gets user input
+# Gets user input
 input("Press ENTER to continue...")
 length = int(input("Password Length?"))
 capital_req = int(input("Capital letter requirement number?"))
 num_req = int(input("Number requirement?"))
 special_req = int(input("Special character requirement number?"))
 
-#Creates methods
+# Gets any errors
 def error(length, capital, num, special):
     global length_error
     global capital_error
@@ -40,6 +40,7 @@ def error(length, capital, num, special):
     if (special_req < 0):
         special_error = True
 
+# Randomly makes a section of the password from the specified character set
 def make_password(required_count, character_set):
     global count
     global password
@@ -48,12 +49,13 @@ def make_password(required_count, character_set):
         count += 1
     count = 0
 
+# Shuffles password
 def shuffle(s):
     l = list(s)
     random.shuffle(l)
     return "".join(l)
 
-#Displays error messages
+# Displays error messages
 error(length, capital_req, num_req, special_req)
 if (length_error):
     print("Error! Invalid length!")
@@ -64,14 +66,14 @@ if (num_error):
 if (special_error):
     print("Error! Invalid special character requirement!")
 
-#Determines if there was an error. If there wasn't, then the password is generated
-if (not length_error) and (not capital_error) and (not num_error) and (not special_error):
-    #Generates password
-    make_password(capital_req, uppercase) #Makes uppercase
-    make_password(num_req, nums) #Makes numbers
-    make_password(special_req, special_char) #Majes special characters
-    make_password(length - len(password), lowercase) #Makes lowercase
-    #Displays the password
+# Determines if there was an error. If there wasn't, then the password is generated
+if ((not length_error) and (not capital_error) and (not num_error) and (not special_error)):
+    # Generates password
+    make_password(capital_req, uppercase) # Makes uppercase
+    make_password(num_req, nums) # Makes numbers
+    make_password(special_req, special_char) # Makes special characters
+    make_password(length - len(password), lowercase) # Makes lowercase
+    # Displays the password
     print("PASSWORD: %s" % shuffle(password))
 else:
     print("Please fix any errors.")
